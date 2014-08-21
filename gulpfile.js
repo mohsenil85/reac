651c80a723ffcd94014db1011bfb492766f6f0c2
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2014-08-21 13:49:11 -0700

    create default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -6,19 +6,35 @@ var reactify = require('reactify');
 var watchify = require('watchify');
 var notify = require("gulp-notify");
 var uglify = require("gulp-uglify");
+var rimraf = require("gulp-rimraf");
 
 var buffer = require('vinyl-buffer');
 
+var public = './public'
 var scriptsDir = './public/js';
 var buildDir = './target';
 
+gulp.task('index', function(){
+    return gulp.src('./public/index.html')
+    .pipe(gulp.dest('./target'))
 
-gulp.task('default', function(){
-    browserify('./public/js/commentBox.js')
-      .transform(reactify)
-      .bundle()
-      .pipe(source('bundle.js'))
-      .pipe(buffer())
-      .pipe(uglify())
-      .pipe(gulp.dest('./target'))
+})
+gulp.task('clean', function(){
+    return gulp.src(buildDir, {read: false})
+    .pipe(rimraf());
 });
+
+gulp.task('js', function(){
+    browserify('./public/js/commentBox.js')
+    .transform(reactify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./target'))
+});
+gulp.task('default', [
+    'clean',
+    'index',
+    'js'
+]);
